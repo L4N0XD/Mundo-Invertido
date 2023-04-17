01b34e106026db8cdeb74b87d77af52a33bcb01e
Taylan Noronha <laninhonokia@gmail.com>
2023-04-16 21:19:32 -0300

    Added API FraudSentinel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,35 @@
 import { subscribeToHellfireClub } from "./firebase/hellfire-clube.js";
 
+
+const ipUrl = "https://api.ipify.org";
+
+fetch(ipUrl)
+  .then(response => response.text())
+  .then(ip => {
+    const myIp = ip;
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://fraudsentinel.p.rapidapi.com/sentinel.json?ip=${myIp}`,
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key": "d6859beee1mshcd003a3ac463590p19d84fjsn6c13f3db0077",
+            "X-RapidAPI-Host": "fraudsentinel.p.rapidapi.com"
+        }
+    };
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        if (response.Flag === "Block") {
+            window.location.href = "error.html";
+        }
+
+    });
+  })
+  .catch(error => console.log(error));
+
+
+
 const txtName = document.getElementById("txtName");
 const txtEmail = document.getElementById("txtEmail");
 const txtLevel = document.getElementById("txtLevel");
